8041d0194d17b9735a8e811ebc7d3cda6900af7f
oladraxler01 <olasukanmitayos@gmail.com>
2024-10-05 13:00:43 +0100

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,34 @@
+
+#import math
+#print(math.pow (2,5))
+#print(math.sqrt(2))
+#import random
+#from math import floor
+#import random as rand 
+#print(dir(random))
+#print(floor(rand.random() *10) + 1)
+#print (rand.randint(1,10))
+
+
+# from math import * #: useed to import everything not usually god
+# # from math import (floor,ceil) #:specify what you need to import
+# # print (floor(2.5))
+# # print(ceil(2.5))
+
+# import random as ran
+# names = ["ola","david","suks","sampo","vinicius","olaunks"]
+# # guess = ran.choice(names)
+# # guess =ran.sample(names,2)
+# # print(guess)
+# bias = [1,5,3,4,9,7]
+# print(ran.choices(names,bias,k=5))
+
+
+# time modules
+import time as t
+#print(t.time())
+#print(t.localtime())
+print(t.strftime("%Y-%m-%d  %H:%M:%S"))
+print("hello")
+t.sleep(10)
+print("welcome")
